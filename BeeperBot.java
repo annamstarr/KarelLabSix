aee4637dfe3c63baf409a6d87b8c07f9d8902ae4
annamstarr <31353295+annamstarr@users.noreply.github.com>
2017-09-25 12:05:44 -0700

    File updated in class

--- BeeperBot.java
+++ BeeperBot.java
@@ -3,7 +3,7 @@ import kareltherobot.*;
 
 
 /**
- * @author :  D. Appel
+ * @author :  A. Starr
  */
 public class BeeperBot extends Robot
 {
@@ -23,6 +23,59 @@ public class BeeperBot extends Robot
             //This will be used to print the beepers found.  No need to modify.
             return beeperList;
     }
-   
+    
+    
+    public void runRace() {
+        while (frontIsClear()) {
+            collectBeeperSteeple();
+        }
+    }
+    public void collectBeeperSteeple() {
+        findBeepers();
+        }
+        public void findBeepers() {
+        faceEast();
+        while (!nextToABeeper() && (frontIsClear())) {
+            move();
+        }
+        collectColumn();
+        resetPosition();
+        faceEast();
+    }
+    public void collectColumn() {
+        faceNorth();
+        while (nextToABeeper()) {
+            pickBeeper();
+            tempCount++;
+            move();
+            }
+        column [0]= tempCount
+        resetTempCount();
+    }
+    public void faceNorth() {
+        while (!facingNorth()) {
+            turnLeft();
+        }
+    }
+    public void faceSouth() {
+        while(!facingSouth()) {
+               turnLeft();
+            }
+        }
+        public void faceEast() {
+        while (!facingEast()) {
+            turnLeft();
+        }
+    }
+    public void resetPosition() {
+        faceSouth();
+        while (frontIsClear()) {
+            move();
+        }
+    }
+    public void resetTempCount() {
+        tempcount= 0
+    }
+    
 }
 
